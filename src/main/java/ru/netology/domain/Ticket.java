9172510193270f75f05e0028ee7c1bba0b934b62
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2022-06-19 15:42:08 +0800

    initial commit

---
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -0,0 +1,21 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Ticket implements Comparable<Ticket> {
+    int id;
+    int cost;
+    String departureAirport;
+    String arrivalAirport;
+    int travelTime;
+
+    @Override
+    public int compareTo(Ticket o) {
+        return 0;
+    }
+}
